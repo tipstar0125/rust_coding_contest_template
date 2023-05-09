9618648527ad6a4e6bd58b680dec078303673142
Yoke <06106186@gmail.com>
2023-05-09 14:21:14 +0900

    original fastio

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,9 @@ const MOD: usize = 1e9 as usize + 7;
 struct Solver {}
 impl Solver {
     fn solve(&mut self) {
-        // let S = read::<String>().chars().collect::<Vec<char>>();
-        // let T: usize = read();
-        // for _ in 0..T {
-        //     let N: usize = read();
-        // }
-        let N: usize = read();
-        println!("{}", N);
+        input! {
+            
+        }
     }
 }
 
@@ -36,17 +32,149 @@ fn main() {
         .unwrap();
 }
 
-fn read<T: std::str::FromStr>() -> T {
-    let mut s = String::new();
-    std::io::stdin().read_line(&mut s).ok();
-    s.trim().parse().ok().unwrap()
+// fn read<T: std::str::FromStr>() -> T {
+//     let mut s = String::new();
+//     std::io::stdin().read_line(&mut s).ok();
+//     s.trim().parse().ok().unwrap()
+// }
+
+// fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+//     read::<String>()
+//         .split_whitespace()
+//         .map(|e| e.parse().ok().unwrap())
+//         .collect()
+// }
+
+#[macro_export]
+macro_rules! input {
+    () => {};
+    (mut $var:ident: $t:tt, $($rest:tt)*) => {
+        let mut $var = __input_inner!($t);
+        input!($($rest)*)
+    };
+    ($var:ident: $t:tt, $($rest:tt)*) => {
+        let $var = __input_inner!($t);
+        input!($($rest)*)
+    };
+    (mut $var:ident: $t:tt) => {
+        let mut $var = __input_inner!($t);
+    };
+    ($var:ident: $t:tt) => {
+        let $var = __input_inner!($t);
+    };
 }
 
-fn read_vec<T: std::str::FromStr>() -> Vec<T> {
-    read::<String>()
-        .split_whitespace()
-        .map(|e| e.parse().ok().unwrap())
-        .collect()
+#[macro_export]
+macro_rules! __input_inner {
+    (($($t:tt),*)) => {
+        ($(__input_inner!($t)),*)
+    };
+    ([$t:tt; $n:expr]) => {
+        (0..$n).map(|_| __input_inner!($t)).collect::<Vec<_>>()
+    };
+    ([$t:tt]) => {{
+        let n = __input_inner!(usize);
+        (0..n).map(|_| __input_inner!($t)).collect::<Vec<_>>()
+    }};
+    (chars) => {
+        __input_inner!(String).chars().collect::<Vec<_>>()
+    };
+    (bytes) => {
+        __input_inner!(String).into_bytes()
+    };
+    (usize1) => {
+        __input_inner!(usize) - 1
+    };
+    ($t:ty) => {
+        $crate::read::<$t>()
+    };
+}
+
+#[macro_export]
+macro_rules! println {
+    () => {
+        $crate::write(|w| {
+            use std::io::Write;
+            std::writeln!(w).unwrap()
+        })
+    };
+    ($($arg:tt)*) => {
+        $crate::write(|w| {
+            use std::io::Write;
+            std::writeln!(w, $($arg)*).unwrap()
+        })
+    };
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => {
+        $crate::write(|w| {
+            use std::io::Write;
+            std::write!(w, $($arg)*).unwrap()
+        })
+    };
+}
+
+#[macro_export]
+macro_rules! flush {
+    () => {
+        $crate::write(|w| {
+            use std::io::Write;
+            w.flush().unwrap()
+        })
+    };
+}
+
+fn read<T>() -> T
+where
+    T: std::str::FromStr,
+    T::Err: std::fmt::Debug,
+{
+    use std::cell::RefCell;
+    use std::io::*;
+
+    thread_local! {
+        pub static STDIN: RefCell<StdinLock<'static>> = RefCell::new(stdin().lock());
+    }
+
+    STDIN.with(|r| {
+        let mut r = r.borrow_mut();
+        let mut s = vec![];
+        loop {
+            let buf = r.fill_buf().unwrap();
+            if buf.is_empty() {
+                break;
+            }
+            if let Some(i) = buf.iter().position(u8::is_ascii_whitespace) {
+                s.extend_from_slice(&buf[..i]);
+                r.consume(i + 1);
+                if !s.is_empty() {
+                    break;
+                }
+            } else {
+                s.extend_from_slice(buf);
+                let n = buf.len();
+                r.consume(n);
+            }
+        }
+        std::str::from_utf8(&s).unwrap().parse().unwrap()
+    })
+}
+
+pub fn write<F>(f: F)
+where
+    F: FnOnce(&mut std::io::BufWriter<std::io::StdoutLock>),
+{
+    use std::cell::RefCell;
+    use std::io::*;
+
+    thread_local! {
+        pub static STDOUT: RefCell<BufWriter<StdoutLock<'static>>> =
+            RefCell::new(BufWriter::new(stdout().lock()));
+    }
+
+    STDOUT.with(|w| f(&mut w.borrow_mut()))
 }
 
 trait Bound<T> {
